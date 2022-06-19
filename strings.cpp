6268e47c1a3bb800fffa76ed16231ad539c71a4b
Anusha Badami <68511029+AnushaBadami@users.noreply.github.com>
2022-06-19 19:18:00 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -36,7 +36,20 @@ int main()
 
 
 
+// reverse the string
 
+
+void reverseString(char[] s) {
+   int left = 0;
+   int end = s.length-1;
+   while (left < end) {
+      
+      char temp = s[left];
+      s[left] = s[end];
+      s[end] = temp;
+      
+      left ++;
+      end --;
 
 
 
