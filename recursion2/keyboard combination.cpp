652f303b9c73c3ad9f97bb0c1ba2e51178221533
Anusha Badami <68511029+AnushaBadami@users.noreply.github.com>
2022-06-26 15:54:26 +0530

    Add files via upload

---
+++ recursion2/keyboard combination.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string key[]={" ", " ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+void printKeypad(int num , string str=""){
+    
+    if(num==0)
+    {
+        cout<<str<<endl;
+        return;
+    }
+    int x=num%10;
+    for(int i=0;i<key[x].size();i++)
+    {
+        printKeypad(num/10,key[x][i]+str);
+    }
+    
+}
+
+
+#include <iostream>
+#include <string>
+#include "solution.h"
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+
+    printKeypad(num);
+
+    return 0;
+}
