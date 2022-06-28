34782f0dabdc3d8c36fdf36e088efb2745d71000
Anusha Badami <68511029+AnushaBadami@users.noreply.github.com>
2022-06-28 17:02:48 +0530

    Update reverse string.cpp

--- strings/reverse string.cpp
+++ strings/reverse string.cpp
@@ -14,24 +14,4 @@ void reverseString(char[] s) {
 
 
 
-// print duplicate characters from given string
 
-#include <iostream>
-#include<string.h>
-using namespace std;
-int main()
-{
-    char string[100];
-    cout<<"Enter string: ";
-    cin.getline(string,100);
-    cout<<"Duplicate characters "<<endl;
-    for(int i=0;i<strlen(string);i++){                   
-        for(int j=i+1;j<strlen(string);j++){
-            if(string[i]==string[j]){                   
-                cout<<string[i]<<endl;                  
-                break;
-            }
-        }
-    }
-    return 0;
-}
