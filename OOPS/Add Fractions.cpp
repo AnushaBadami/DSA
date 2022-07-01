b20a2c7543b1c281740dc72415030c1e6b435d1f
Anusha Badami <68511029+AnushaBadami@users.noreply.github.com>
2022-07-01 22:31:55 +0530

    Create Add Fractions.cpp

---
+++ OOPS/Add Fractions.cpp
@@ -0,0 +1,48 @@
+class Fraction {
+	private :
+
+		int numerator;
+		int denominator;
+
+	public :
+
+		Fraction(int numerator, int denominator) {
+			this -> numerator = numerator;
+			this -> denominator = denominator;
+		}
+
+		void print() {
+			cout << this -> numerator << " / " << denominator << endl;
+		}
+
+
+		
+
+		void add(Fraction f2) {
+			int lcm = denominator * f2.denominator;
+			int x = lcm / denominator;
+			int y = lcm / f2.denominator;
+
+			int num = x * numerator + (y * f2.numerator);
+
+			numerator = num;
+			denominator = lcm;
+
+			
+		}
+};
+
+
+#include <iostream>
+using namespace std;
+#include "Fraction.cpp"
+
+int main() {
+	Fraction f1(10, 2);
+	Fraction f2(15, 4);
+
+	f1.add(f2);
+
+	f1.print();
+	f2.print();
+}
